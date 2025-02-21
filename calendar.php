d23f07f183b96606346495377632c7a460d20eb5
GIZMO08 <ahmanii.peoples@example.com>
2025-02-21 17:09:25 -0500

    updated names

--- calendar.php
+++ calendar.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Calender</title>
+    <title>Calendar</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
