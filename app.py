981d65d764a034b122ab7b045e92e3333736ca8a
GIZMO08 <ahmanii.peoples@example.com>
2025-03-07 16:19:04 -0500

    Changed port

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def clear_session():
     return jsonify({'status': 'session cleared'})
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
